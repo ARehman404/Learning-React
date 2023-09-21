96a9392ef923f64e0516560c064961914552cbac
Abdul Rehman <abdulrehman@Equalss-MacBook-Pro.local>
2023-09-21 13:12:04 +0100

    task 1.3 - done

--- my-app/src/Greetings.js
+++ my-app/src/Greetings.js
@@ -1,11 +1,17 @@
+import { useState } from "react";
 
-
-function Greetings(prop) {
+export default function Greetings(prop) {
+  const [count, setCount] = useState(0)
+  function Counter(){
+    setCount(count+1)
+  }
   return (
     <>
       <h1>Hello, {prop.name}</h1>
+      <button onClick={Counter}>
+        Click me {count} times
+      </button>
+
     </>
   );
 }
-
-export default Greetings;
